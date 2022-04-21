75cf99f4ed0d4e577fa878e485d876edb72854d9
briglass <briglass@gmail.com>
2022-04-21 10:07:25 -0700

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,53 +10,85 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Guess the word in 3 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the word. Green means the right letter is in the right spot. Yellow means the letter is in the word, but not in the right spot. 90% of the time, the answer is Jeff Goldblum. 10% of the time, the answer is not Jeff Goldblum.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="J"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell
+           <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="E"
+          status="correct"
+        />
+           <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="F"
+          status="correct"
+        />
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="F"
+          status="correct"
+        />
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="G"
+          status="correct"
+        />        
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="O"
+          status="correct"
+        />
+            <Cell
           isRevealing={true}
           isCompleted={true}
           value="L"
-          status="present"
+          status="correct"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="D"
+          status="correct"
+        />
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="B"
+          status="correct"
+        />
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="L"
+          status="correct"
+        />        
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="U"
+          status="correct"
+        />
+            <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="M"
+          status="correct"
+        />
       </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
-      </p>
-
+    
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is an open source version of the word guessing game we all know and
         love -{' '}
