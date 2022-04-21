430f04c8666dfe28d20daa206340e68e3248ad7c
briglass <briglass@gmail.com>
2022-04-21 10:20:35 -0700

    Update share.ts

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -21,7 +21,7 @@ export const shareStatus = (
     `${GAME_TITLE} ${solutionIndex} ${
       lost ? 'X' : guesses.length
     }/${MAX_CHALLENGES}${isHardMode ? '*' : ''}\n\n` +
-    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode))
+    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode)) + `\nhttps://jeffgoldblumle.vercel.app/`
 
   const shareData = { text: textToShare }
 
