45a95063f9254683cbb012c06d2059a835900da6
briglass <briglass@gmail.com>
2022-04-21 16:03:26 -0700

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -24,8 +24,6 @@ export const Navbar = ({
           onClick={() => setIsInfoModalOpen(true)}
         />
         <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>        
-        <p className="text-m ml-2.5 font-bold dark:text-white">90% of the time, the answer is Jeff Goldblum</p>
-        
         <div className="right-icons">
           <ChartBarIcon
             className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
