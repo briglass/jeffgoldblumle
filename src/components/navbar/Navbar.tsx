346d20dd8da4bb4d61e83cece63cee0c6bfc4686
briglass <briglass@gmail.com>
2022-04-21 10:57:23 -0700

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -24,10 +24,9 @@ export const Navbar = ({
           onClick={() => setIsInfoModalOpen(true)}
         />
         <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
+        <p className="text-m ml-2.5 font-bold dark:text-white"><a href="https://www.buymeacoffee.com/briglass314">test</a></p>      
         <p className="text-m ml-2.5 font-bold dark:text-white">90% of the time, the answer is Jeff Goldblum</p>
-                <p>
-          <a href="https://www.buymeacoffee.com/briglass314"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee"></a>
-        </p>
+        
         <div className="right-icons">
           <ChartBarIcon
             className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
