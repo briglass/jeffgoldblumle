535a3232503bfd337b85f297951aba925adf934d
briglass <briglass@gmail.com>
2026-05-22 13:22:45 -0700

    space fixes

--- src/App.tsx
+++ src/App.tsx
@@ -255,7 +255,7 @@ function App() {
         setIsSettingsModalOpen={setIsSettingsModalOpen}
       />
       <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
-        <div className="pb-6 grow">
+        <div className="pb-6 grow flex items-center justify-center">
           <Grid
             guesses={guesses}
             currentGuess={currentGuess}
@@ -264,7 +264,7 @@ function App() {
           />
         </div>
 
-        <div className="pb-6 grow justify-center">
+        <div className="pb-4 flex justify-center">
           <p className="text-m font-medium text-black-100 text-center p-0.5 dark:text-white">
             90% of the time, the answer is Jeff Goldblum.
           </p>
