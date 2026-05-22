508f19bd05d45ced7631baff245756e7ff165544
briglass <briglass@gmail.com>
2026-05-22 13:26:35 -0700

    grid fix!

--- src/App.tsx
+++ src/App.tsx
@@ -255,7 +255,7 @@ function App() {
         setIsSettingsModalOpen={setIsSettingsModalOpen}
       />
       <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
-        <div className="pb-6 grow flex items-center justify-center">
+        <div className="pb-6 grow flex flex-col justify-center">
           <Grid
             guesses={guesses}
             currentGuess={currentGuess}
