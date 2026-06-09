5768b89d65299777169e02fa1a2f0cc9a3a5a081
briglass <briglass@gmail.com>
2026-06-08 20:19:58 -0700

    monetag changes

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11120869
+    "zoneId": 11120980
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
