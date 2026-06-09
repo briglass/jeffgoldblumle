39970a15c6acbf410721869d095784569221d225
briglass <briglass@gmail.com>
2026-06-09 15:17:23 -0700

    get rid of direct link ad

--- src/App.tsx
+++ src/App.tsx
@@ -318,14 +318,14 @@ function App() {
               </a>
             </div>
 
-            <a
+            {/* <a
               href="https://omg10.com/4/11121017"
               target="_blank"
               rel="noopener noreferrer"
               className="px-3 py-1.5 bg-gradient-to-tr from-emerald-400 via-teal-500 to-cyan-600 text-white text-sm font-bold rounded shadow hover:opacity-90 transition-opacity"
             >
               Check out our sponsor
-            </a>
+            </a> */}
           </div>
         </div>
 
