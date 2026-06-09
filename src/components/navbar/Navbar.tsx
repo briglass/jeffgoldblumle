3fe0d9d1bc156479b65eda9192dd86c569306483
briglass <briglass@gmail.com>
2026-06-08 20:33:32 -0700

    navbar change and monetag ads

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -19,12 +19,7 @@ export const Navbar = ({
   return (
     <div className="navbar">
       <div className="navbar-content px-5">
-        <InformationCircleIcon
-          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
-          onClick={() => setIsInfoModalOpen(true)}
-        />
-        <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
-        <div className="right-icons">
+        <div className="flex">
           <ChartBarIcon
             className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
             onClick={() => setIsStatsModalOpen(true)}
@@ -32,6 +27,13 @@ export const Navbar = ({
           <CogIcon
             className="h-6 w-6 cursor-pointer dark:stroke-white"
             onClick={() => setIsSettingsModalOpen(true)}
+          />
+        </div>
+        <p className="text-xl font-bold dark:text-white">{GAME_TITLE}</p>
+        <div className="right-icons">
+          <InformationCircleIcon
+            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            onClick={() => setIsInfoModalOpen(true)}
           />
         </div>
       </div>
