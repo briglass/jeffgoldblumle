2b70bd38d5e157393cd830eb1bb467b4aca3a4da
briglass <briglass@gmail.com>
2026-08-05 11:18:16 -0700

    ad free for content creators

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -60,7 +60,7 @@ export const StatsModal = ({
           onClick={onCreatorInfoClick}
           className="text-xs font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 underline transition-colors"
         >
-          Want ad-free for free? Learn more
+          Ad-free access for Content Creators!
         </button>
       </div>
     </div>
